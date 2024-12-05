43ba5ea15f693bae1b76e49a8b223a4d6534f7fb
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-12-05 21:12:03 +0900

    Use for loops to show 2015-Feb calendar

--- mycal.c
+++ mycal.c
@@ -3,11 +3,14 @@
 #include "calendar.h"
 
 int main(void) {
-	puts("    February 2015");
-	puts("Su Mo Tu We Th Fr Sa");
-	puts(" 1  2  3  4  5  6  7");
-	puts(" 8  9 10 11 12 13 14");
-	puts("15 16 17 18 19 20 21");
-	puts("22 23 24 25 26 27 28");
+	puts("     February 2015");
+	puts(" Su Mo Tu We Th Fr Sa");
+	for (int week = 0; week < 4; ++week) {
+		for (int i = 1; i <= 7; ++i) {
+			int const day = week * 7 + i;
+			printf(" %2d", day);
+		}
+		printf("\n");
+	}
 	return 0;
 }
