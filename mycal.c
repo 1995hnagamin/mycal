483be9f636b9d74ffb99f5b81e0d541cd85c3360
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-12-05 21:25:32 +0900

    Introduce day_cells[] to mycal.c

--- mycal.c
+++ mycal.c
@@ -2,13 +2,28 @@
 #include <stdio.h>
 #include "calendar.h"
 
+char const *day_cells[] = {
+	"  ", "  ", "  ", "  ", "  ", "  ",
+	" 1", " 2", " 3", " 4", " 5", " 6", " 7",
+	" 8", " 9", "10", "11", "12", "13", "14",
+	"15", "16", "17", "18", "19", "20", "21",
+	"22", "23", "24", "25", "26", "27", "28",
+	"29", "30", "31",
+};
+
 int main(void) {
+	int const year = 2015;
+	int const month = 2;
+	dow_t first_dow = calc_dow(year, month, 1);
+	int cell_offset = 13 - first_dow;
+	char const *(*cells)[7] =
+		(char const *(*)[7])(day_cells + cell_offset);
+
 	puts("     February 2015");
 	puts(" Su Mo Tu We Th Fr Sa");
 	for (int week = 0; week < 4; ++week) {
-		for (int i = 1; i <= 7; ++i) {
-			int const day = week * 7 + i;
-			printf(" %2d", day);
+		for (int i = 0; i < 7; ++i) {
+			printf(" %s", cells[week][i]);
 		}
 		printf("\n");
 	}
