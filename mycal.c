2849e75adb5bbb799d4938a3d14139dba57034bb
Nagamine, Hideaki <nagamine.hideaki.88x@kyoto-u.jp>
2024-12-05 21:09:47 +0900

    Show 2015-Feb calendar

--- mycal.c
+++ mycal.c
@@ -3,8 +3,11 @@
 #include "calendar.h"
 
 int main(void) {
-	puts("Hello, World!");
-	dow_t dow = calc_dow(2024, 12, 5);
-	printf("%d\n", dow);
+	puts("    February 2015");
+	puts("Su Mo Tu We Th Fr Sa");
+	puts(" 1  2  3  4  5  6  7");
+	puts(" 8  9 10 11 12 13 14");
+	puts("15 16 17 18 19 20 21");
+	puts("22 23 24 25 26 27 28");
 	return 0;
 }
